086c17fa5849ff3895cc6dee76ace1480c2da051
linfenpan <linfenpan@corp.netease.com>
2019-10-21 12:03:38 +0800

    python 执行 js 代码

---
+++ learn/python/javascript/run1.py
@@ -0,0 +1,21 @@
+#!/user/bin/env python
+# coding: utf-8
+
+# 安装一下 PyExecJS
+import execjs
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# 打开 js 文件，并读取内容
+f = open("./run1.js", "r")
+line = f.readline()
+jsstr = ''
+while line:
+  jsstr = jsstr + line
+  line = f.readline()
+
+# 执行 js 内容，调用 run.js 中的 run 方法
+ctx = execjs.compile(jsstr)
+print ctx.call('run', 'da黑熊')
---
+++ learn/python/javascript/spider.py
@@ -0,0 +1,2 @@
+# 参考 https://blog.csdn.net/ychgyyn/article/details/83474817
+selenium
